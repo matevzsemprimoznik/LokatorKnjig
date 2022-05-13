51074f0cbe06ef22735ffdc316f51efba04f3130
zalalahovnik <zala.lahovnik@student.um.si>
2022-05-13 18:56:29 +0200

    Added textGeometry to display UDK of each shelf

--- src/3dcomponent/BookshelfPiece.tsx
+++ src/3dcomponent/BookshelfPiece.tsx
@@ -5,6 +5,10 @@ import bookshelf from '../assets/bookshelf.glb';
 import '@babylonjs/loaders/glTF';
 import { GUI } from 'dat.gui';
 import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry';
+import Roboto from '../assets/Roboto_Bold.json';
+import { extend } from 'react-three-fiber';
 
 interface BookshelfPieceProps {
   position: {
@@ -17,23 +21,38 @@ interface BookshelfPieceProps {
     y: number;
     z: number;
   };
+  udk: Array<number>;
 }
+
+extend({TextGeometry});
 
 const BookshelfPiece: FC<BookshelfPieceProps> = ({
   position,
   rotation = { x: 0, y: 0, z: 0 },
+  udk
 }) => {
   const { nodes, materials }: any = useGLTF(bookshelf);
 
+  const font = new FontLoader().parse(Roboto);
+
   return (
-    <mesh
-      castShadow
-      receiveShadow
-      geometry={nodes.Cube.geometry}
-      material={materials['CubeMaterial']}
-      position={[position.x, position.y, position.z]}
-      rotation={[rotation.x, rotation.y, rotation.z]}
-    />
+    <>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube.geometry}
+        material={materials['CubeMaterial']}
+        position={[position.x, position.y, position.z]}
+        rotation={[rotation.x, rotation.y, rotation.z]}
+      />
+      <mesh 
+        position={[position.x,position.y,position.z]} 
+        rotation={[rotation.x, rotation.y, rotation.z]}
+      >
+        {/* @ts-ignore*/}
+        <textGeometry args={[`${udk[0]}`, {font, size:0.1, height:0}]} />
+      </mesh>
+    </>
   );
 };
 
--- src/3dcomponent/SelectedBookshelfPiece.tsx
+++ src/3dcomponent/SelectedBookshelfPiece.tsx
@@ -5,6 +5,10 @@ import bookshelf from '../assets/selectedBookshelf.glb';
 import '@babylonjs/loaders/glTF';
 import { GUI } from 'dat.gui';
 import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry';
+import Roboto from '../assets/Roboto_Bold.json';
+import { extend } from 'react-three-fiber';
 
 interface SelectedBookshelfPieceProps {
   position: {
@@ -17,15 +21,22 @@ interface SelectedBookshelfPieceProps {
     y: number;
     z: number;
   };
+  udk: Array<number>;
 }
+
+extend({TextGeometry});
 
 const SelectedBookshelfPiece: FC<SelectedBookshelfPieceProps> = ({
   position,
   rotation = { x: 0, y: 0, z: 0 },
+  udk
 }) => {
   const { nodes, materials }: any = useGLTF(bookshelf);
 
+  const font = new FontLoader().parse(Roboto);
+
   return (
+    <>
     <mesh
       castShadow
       receiveShadow
@@ -34,6 +45,14 @@ const SelectedBookshelfPiece: FC<SelectedBookshelfPieceProps> = ({
       position={[position.x, position.y, position.z]}
       rotation={[rotation.x, rotation.y, rotation.z]}
     />
+    <mesh 
+      position={[position.x,position.y,position.z]} 
+      rotation={[rotation.x, rotation.y, rotation.z]}
+    >
+      {/* @ts-ignore*/}
+      <textGeometry args={[`${udk[0]}`, {font, size:0.1, height:0}]} />
+    </mesh>
+    </>
   );
 };
 
