9415bbff744c762351a5c231b4d014ef96bb33cc
Matevz <semprimoznik.matevz@gmail.com>
2022-05-13 09:44:50 +0200

    render model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Grid from './components/Grid';
+import Model from './pages/Model';
+import { Controls, useControl } from 'react-three-gui';
+import { Canvas } from 'react-three-fiber';
 
 function App() {
   return (
     <div>
-      <Grid />
+      <Canvas camera={{ position: [0, 5, 10], fov: 60 }}>
+        <Model />
+      </Canvas>
     </div>
   );
 }
---
+++ src/constants/constants.ts
@@ -0,0 +1,1 @@
+export const BOOKSHELF_PIECE_SIZE = { x: 2, y: 0.64, z: 0.5 };
