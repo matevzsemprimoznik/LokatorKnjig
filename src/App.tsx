71fa26a9603b36b66f69f7661761f90f3ab89572
Matevz <semprimoznik.matevz@gmail.com>
2022-05-20 08:45:32 +0200

    add bird's eye view

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Model from "./pages/Model";
 import { Controls, useControl } from "react-three-gui";
 import { Canvas } from "react-three-fiber";
 import Model2D from "./3DComponents/Model2D";
+import { ModelShape } from "@babylonjs/core/Particles/solidParticle";
 
 function App() {
   return (
     <div>
-      <Canvas camera={{ position: [0, 5, 10], fov: 60 }}>
-        <Model2D />
+      <Canvas>
+        <Model />
       </Canvas>
     </div>
   );
--- src/pages/Model.tsx
+++ src/pages/Model.tsx
@@ -1,23 +1,94 @@
-import { MeshWobbleMaterial, OrbitControls } from "@react-three/drei";
-import { useLoader, Canvas } from "@react-three/fiber";
-import data from "../data.json";
-import Floor from "../3DComponents/Floor";
-import SelectedBookshelfPiece from "../3DComponents/SelectedBookshelfPiece";
-import BookshelfPiece from "../3DComponents/BookshelfPiece";
-import { useThree } from "@react-three/fiber";
+import { MeshWobbleMaterial, OrbitControls, OrthographicCamera, PerspectiveCamera } from '@react-three/drei';
+import { useLoader, Canvas, useFrame, PerspectiveCameraProps, OrthographicCameraProps } from '@react-three/fiber';
+import data from '../data.json';
+import Floor from '../3DComponents/Floor';
+import SelectedBookshelfPiece from '../3DComponents/SelectedBookshelfPiece';
+import BookshelfPiece from '../3DComponents/BookshelfPiece';
+import { useThree } from '@react-three/fiber';
+import { FC, memo, useEffect, useRef, useState } from 'react';
+import { OrbitControls as OrbitControlsProps } from 'three-stdlib';
+import * as THREE from 'three';
+
+let nekaj = false;
 
 const Model = () => {
   const selectedUDK = 20;
-  useThree(({ camera }) => {
-    camera.rotation.set(Math.PI / 3, 0, 0);
+  const perspectiveCameraRef = useRef<PerspectiveCameraProps>(null);
+  const triggerSwitchFrom3dTo2d = useRef(false);
+  const orbitControlsRef = useRef<OrbitControlsProps>(null);
+  const [defualtCamera, setDefaultCamera] = useState<'perspective' | 'ortographic'>('perspective');
+  const pos = useRef({
+    x: null,
+    z: null,
   });
+  useFrame(() => {
+    if (triggerSwitchFrom3dTo2d.current) moveCameraTo2dView(0.5);
+  });
+
+  const moveCameraTo2dView = (speed: number) => {
+    if (
+      orbitControlsRef.current == null ||
+      perspectiveCameraRef.current == null ||
+      perspectiveCameraRef.current.position == null ||
+      !(perspectiveCameraRef.current.position instanceof THREE.Vector3)
+    )
+      return;
+
+    orbitControlsRef.current.enableZoom = false;
+    orbitControlsRef.current.enableRotate = false;
+
+    const position = perspectiveCameraRef.current.position;
+
+    const azimuthalAngle = orbitControlsRef.current.getAzimuthalAngle();
+    const polarAngle = orbitControlsRef.current.getPolarAngle();
+
+    if (Math.abs(azimuthalAngle) < 0.1 && Math.abs(polarAngle) < 0.1 && position?.y > 30) {
+      triggerSwitchFrom3dTo2d.current = false;
+      orbitControlsRef.current.enableZoom = true;
+      console.log('end');
+      setDefaultCamera('ortographic');
+
+      return;
+    }
+    if (azimuthalAngle < -speed) orbitControlsRef.current.setAzimuthalAngle(azimuthalAngle + speed);
+    else if (azimuthalAngle > speed) orbitControlsRef.current.setAzimuthalAngle(azimuthalAngle - speed);
+    if (polarAngle < -speed) orbitControlsRef.current.setPolarAngle(polarAngle + speed);
+    else if (polarAngle > speed) orbitControlsRef.current.setPolarAngle(polarAngle - speed);
+    console.log(Math.abs(azimuthalAngle), polarAngle);
+
+    if (Math.abs(azimuthalAngle) <= speed) {
+      const direction = azimuthalAngle / Math.abs(azimuthalAngle);
+      orbitControlsRef.current.setAzimuthalAngle(
+        Number.isNaN(direction) ? 0 : direction * (Math.abs(azimuthalAngle) - 0.1)
+      );
+    }
+
+    if (Math.abs(polarAngle) <= speed) {
+      const direction = polarAngle / Math.abs(polarAngle);
+      orbitControlsRef.current.setPolarAngle(Number.isNaN(direction) ? 0 : direction * (Math.abs(polarAngle) - 0.1));
+    }
+
+    if (position.y < 30) perspectiveCameraRef.current.position.y += speed;
+  };
+  useEffect(() => {
+    setInterval(() => {
+      triggerSwitchFrom3dTo2d.current = true;
+    }, 5000);
+  }, []);
   return (
     <>
-      <ambientLight intensity={0.3} />
+      {console.log(defualtCamera)}
+      <PerspectiveCamera
+        ref={perspectiveCameraRef}
+        makeDefault={defualtCamera === 'perspective'}
+        position={[0, 5, 10]}
+      />
+      <OrthographicCamera makeDefault={defualtCamera === 'ortographic'} position={[0, 10, 0]} zoom={32} />
+      <ambientLight intensity={defualtCamera === 'ortographic' ? 1.3 : 0.3} />
       <directionalLight
         castShadow
         position={[5, 10, 0]}
-        intensity={1.5}
+        intensity={1}
         shadow-mapSize-width={1024}
         shadow-mapSize-height={1024}
         shadow-camera-far={50}
@@ -26,13 +97,83 @@ const Model = () => {
         shadow-camera-top={10}
         shadow-camera-bottom={-10}
       />
-      <pointLight position={[-10, 0, -20]} intensity={0.5} />
-      <pointLight position={[0, -10, 0]} intensity={1.5} />
+      {defualtCamera === 'perspective' && (
+        <>
+          <pointLight position={[-10, 0, -20]} intensity={0.5} />
+          <pointLight position={[0, -10, 0]} intensity={1.5} />
+        </>
+      )}
 
-      {data.police.map((polica: any) =>
+      {defualtCamera === 'perspective' ? (
+        <MemoizedBookshelfs selectedUDK={selectedUDK} />
+      ) : (
+        data.police.map((polica: any, index: number) => {
+          if (polica.udk.includes(selectedUDK)) {
+            return (
+              <mesh key={index}>
+                <mesh position={[polica.pozicija.x, 0.1, polica.pozicija.z]}>
+                  <boxGeometry attach='geometry' args={[2, 0, 0.6]} />
+                  <meshStandardMaterial attach='material' color={'E6EFE9'} />
+                </mesh>
+                <mesh position={[polica.pozicija.x, 0.102, polica.pozicija.z]} scale={[0.98, 1, 0.8]}>
+                  <boxGeometry attach='geometry' args={[2, 0, 0.6]} />
+                  <meshStandardMaterial attach='material' color={'#3574AE'} />
+                </mesh>
+              </mesh>
+            );
+          }
+
+          if (JSON.stringify(pos.current) !== JSON.stringify({ x: polica.pozicija.x, z: polica.pozicija.z })) {
+            pos.current = { x: polica.pozicija.x, z: polica.pozicija.z };
+            return (
+              <mesh key={index}>
+                <mesh position={[polica.pozicija.x, 0.1, polica.pozicija.z]}>
+                  <boxGeometry attach='geometry' args={[2, 0, 0.6]} />
+                  <meshStandardMaterial attach='material' color={'E6EFE9'} />
+                </mesh>
+                <mesh position={[polica.pozicija.x, 0.101, polica.pozicija.z]} scale={[0.98, 1, 0.8]}>
+                  <boxGeometry attach='geometry' args={[2, 0, 0.6]} />
+                  <meshStandardMaterial attach='material' color={'#AE7C44'} />
+                </mesh>
+              </mesh>
+            );
+          }
+        })
+      )}
+
+      <Floor position={{ x: 0, y: 0.05, z: 0 }} />
+      <OrbitControls
+        ref={orbitControlsRef}
+        mouseButtons={
+          defualtCamera === 'ortographic'
+            ? {
+                LEFT: THREE.MOUSE.RIGHT,
+                MIDDLE: THREE.MOUSE.MIDDLE,
+                RIGHT: THREE.MOUSE.LEFT,
+              }
+            : {
+                LEFT: THREE.MOUSE.LEFT,
+                MIDDLE: THREE.MOUSE.MIDDLE,
+                RIGHT: THREE.MOUSE.RIGHT,
+              }
+        }
+      />
+    </>
+  );
+};
+
+export default Model;
+
+interface BookShelfsProps {
+  selectedUDK: number;
+}
+const BookShelfs: FC<BookShelfsProps> = ({ selectedUDK }) => {
+  return (
+    <>
+      {data.police.map((polica: any, index: number) =>
         polica.udk.includes(selectedUDK) ? (
           <SelectedBookshelfPiece
-            key={polica.udk[0]}
+            key={index}
             position={{
               x: polica.pozicija.x,
               y: polica.pozicija.y,
@@ -47,7 +188,7 @@ const Model = () => {
           />
         ) : (
           <BookshelfPiece
-            key={polica.udk[0]}
+            key={index}
             position={{
               x: polica.pozicija.x,
               y: polica.pozicija.y,
@@ -62,12 +203,7 @@ const Model = () => {
           />
         )
       )}
-
-      <Floor position={{ x: 0, y: 0.05, z: 0 }} />
-
-      <OrbitControls />
     </>
   );
 };
-
-export default Model;
+const MemoizedBookshelfs = memo(BookShelfs);
