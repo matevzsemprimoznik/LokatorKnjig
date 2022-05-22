b49643ef13b7d36fa8a20e920859f2cdeea514ff
zalalahovnik <zala.lahovnik@student.um.si>
2022-05-22 10:54:09 +0200

    Added functions to search bar, to navigate to proper page

--- src/components/landing_page/SearchForm.tsx
+++ src/components/landing_page/SearchForm.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import '../../styles/landing_page/SearchForm.css';
 
 const SearchForm = () => {
+    const [searchedUDK, setSearchedUDK] = React.useState("");
+    let history = useNavigate();
+
+    const handleUDKChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchedUDK(e.currentTarget.value);
+    }
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        let link = "/library-model/" + searchedUDK;
+        history(link);
+    }
+
     return (
         <div className="search-form">
             <div className="search-form-container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input
                         type="text"
-                        placeholder="Išči knjižnico..."
+                        placeholder="Išči po UDK..."
+                        onChange={handleUDKChange}
                     />
                     <input type="submit" value="Išči"/>
                 </form>
