e856e692be37bc639c691806626c903b9f5e6be9
DrozgDavorin <davorin.drozg@student.um.si>
2022-05-26 22:53:40 +0200

    add canvas visual changes

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent, useEffect, useLayoutEffect, useRef, useState} from '
 import rough from "roughjs/bin/rough";
 import {RoughCanvas} from "roughjs/bin/canvas";
 import {Drawable} from "roughjs/bin/core";
+import '../styles/2d_canvas_page/Canvas.css';
 
 const generator = rough.generator();
 
@@ -67,6 +68,7 @@ const Canvas = () => {
 
         const [bs_details, setBs_details] = useState<DataType>(bs_initial_details);
         const [drawingBlock, setDrawingBlock] = useState<boolean>(false);
+        const [sh_image, setSh_image] = useState<string>("../../Shelf_rot_to.png");
 
         const [currentDrawingBlock, setCurrentDrawingBlock] = useState<Array<ElementType>>([]);
         const [blockSplitOnPieces, setBlockSplitOnPieces] = useState<Array<ElementType>>([]);
@@ -89,6 +91,26 @@ const Canvas = () => {
 
         }, [wallElements, bookshelves, currentDrawingBookshelf, currentDrawingBlock]);
 
+        useEffect(() => {
+            getImagePath(bs_details.sh_rotation);
+        }, [bs_details.sh_rotation])
+
+        const getImagePath = (rotation: number) => {
+            switch (Number(rotation)) {
+                case 0:
+                    setSh_image("../../Shelf_rot_to.png")
+                    break;
+                case 1:
+                    setSh_image("../../Shelf_rot_left.png")
+                    break;
+                case 2:
+                    setSh_image("../../Shelf_rot_against.png")
+                    break;
+                case 3:
+                    setSh_image("../../Shelf_rot_right.png")
+                    break;
+            }
+        }
 
         const createElement = (id: number, x: number, y: number, x1: number, y1: number, type: DrawingElement, rotation?: number) => {
 
@@ -417,37 +439,79 @@ const Canvas = () => {
             const name = e.target.name;
             const value = e.target.value;
             setBs_details({...bs_details, [name]: value});
+
         }
 
+        const [radio, setRadio] = useState<number | null>(null);
+
+        const handleRadioChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+            setRadio(Number(e.target.value));
+        }
 
         return (
-            <div>
-                <div style={{position: "fixed", maxHeight: "30px", display: "flex", alignItems: "center", zIndex: 1}}>
-                    <button onClick={() => handleDrawingSelection(DrawingElement.WALL)}>Risanje sten</button>
-                    <button onClick={() => setLeftDivOpen(!leftDivOpen)}>Vstavi omaro</button>
-                    <button onClick={handleElementSelection}>Izbira elementov</button>
-                    <button onClick={handleElementDelete}>Izbriši element</button>
-                    <p>ESC == unselect action</p>
+            <>
+                <div className="topDiv">
+                    <div className={`topDiv-element${(radio===0) ? '--checked' : ''}`} onClick={handleElementSelection}>
+                        <input id="radio-select" type="radio" name="action-selection" className="topDiv-elementRadio"
+                               onChange={handleRadioChange} value="0"/>
+                        <label htmlFor="radio-select">
+                        <svg viewBox="0 0 320 512" className={`${(radio===0) ? 'topDiv-element-svgWhite' : 'topDiv-element-svgBlack'}`}>
+                            <path
+                                d="M302.189 329.126H196.105l55.831 135.993c3.889 9.428-.555 19.999-9.444 23.999l-49.165 21.427c-9.165 4-19.443-.571-23.332-9.714l-53.053-129.136-86.664 89.138C18.729 472.71 0 463.554 0 447.977V18.299C0 1.899 19.921-6.096 30.277 5.443l284.412 292.542c11.472 11.179 3.007 31.141-12.5 31.141z"></path>
+                        </svg>
+                    </label>
+                    </div>
+                    <div className={`topDiv-element${(radio===1) ? '--checked' : ''}`} onClick={() => handleDrawingSelection(DrawingElement.WALL)}>
+                        <input id="radio-drawLine" type="radio" name="action-selection" className="topDiv-elementRadio"
+                               onChange={handleRadioChange} value="1"/>
+                        <label htmlFor="radio-drawLine">
+                        <svg viewBox="0 0 6 6" className={`${(radio===1) ? 'topDiv-element-svgWhite' : 'topDiv-element-svgBlack'}`}>
+                            <line x1="0" y1="3" x2="6" y2="3" stroke="currentColor" strokeLinecap="round"></line>
+                        </svg>
+                    </label>
+                    </div>
+                    <div className={`topDiv-element${(radio===2) ? '--checked' : ''}`} onClick={() => setLeftDivOpen(!leftDivOpen)}>
+                        <input id="radio-drawRect" type="radio" name="action-selection" className="topDiv-elementRadio"
+                               onChange={handleRadioChange} value="2"/>
+                        <label htmlFor="radio-drawRect">
+                        <svg viewBox="0 0 448 512" className={`${(radio===2) ? 'topDiv-element-svgWhite' : 'topDiv-element-svgBlack'}`}>
+                            <path
+                                d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z"></path>
+                        </svg>
+                    </label>
+                    </div>
+                    <div className={`topDiv-element`} onClick={handleElementDelete}>
+                        <input id="radio-delete" type="radio" name="action-selection" className="topDiv-elementRadio"
+                               onChange={handleRadioChange} value="3"/>
+                        <label htmlFor="radio-delete">
+                            <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 448 512"
+                                 className={"topDiv-element-svgBlack"}>
+                                <path fill="currentColor"
+                                      d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path>
+                            </svg>
+                        </label>
+                    </div>
                 </div>
                 {leftDivOpen && (
-                    <div style={styles.leftDiv}>
-                        <form onSubmit={handleSubmit} style={styles.form}>
-                            <div style={styles.inputContainer}>
+                    <div className="leftDiv">
+                        <form onSubmit={handleSubmit} className="form">
+                            <div className="inputContainer">
                                 <label htmlFor='ime'>Police v dolžino : </label>
                                 <input name="sh_length" id="sh_rotation" type="number" min="1" value={bs_details.sh_length}
                                        onChange={handleChange}/>
                             </div>
-                            <div style={styles.inputContainer}>
+                            <div className="inputContainer">
                                 <label htmlFor='ime'>Število polic v višino : </label>
                                 <input name="sh_height" id="sh_height" type="number" min="1" value={bs_details.sh_height}
                                        onChange={handleChange}/>
                             </div>
-                            <div style={styles.inputContainer}>
+                            <div className="inputContainer">
                                 <label htmlFor='ime'>Rotacija : </label>
                                 <input name="sh_rotation" id="sh_rotation" type="range" min="0" max="3"
                                        value={bs_details.sh_rotation} step="1" onChange={handleChange}/>
                             </div>
-
+                            <img className="image" src={sh_image} alt="Usmerjenost police"/>
                             <button onClick={() => handleDrawingSelection(DrawingElement.BOOKSHELF)}> Vstavi omaro</button>
                         </form>
                     </div>
@@ -461,39 +525,9 @@ const Canvas = () => {
                         onDoubleClick={handleDoubleClick}
                         style={{backgroundColor: "lightgray"}}
                 ></canvas>
-            </div>
+            </>
         );
     }
 ;
 
 export default Canvas;
-
-const styles: Record<string, object> = {
-    leftDiv: {
-        position: "fixed",
-        top: "3em",
-        left: "0.5em",
-        height: "25em",
-        width: "15em",
-        backgroundColor: "white",
-        borderRadius: "10px",
-        boxShadow: "0 1px 17px 0 gray",
-
-    },
-    inputContainer: {
-        display: "flex",
-        flexDirection: "column",
-        gap: "0.5em"
-    },
-    form: {
-        display: "flex",
-        flexDirection: "column",
-        padding: "1em 2em",
-        gap: "1em"
-    },
-    image: {
-        padding: "0.2em 1.5em 1em 1em",
-        borderRadius: "10px",
-    }
-
-}
