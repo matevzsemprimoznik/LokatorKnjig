18f6a4a91b5cf76b672fa0de37f5ef531e9694a6
zalalahovnik <zala.lahovnik@student.um.si>
2022-05-28 16:35:33 +0200

    Added methods for library data

--- Backend/routes/libraries.js
+++ Backend/routes/libraries.js
@@ -16,4 +16,37 @@ router.post('/', authenticateJWT, async (req, res) => {
   return res.json(result);
 });
 
+//get all libraries (returns section, abbreviation and description of each library)
+router.get('/all', async (req, res) => {
+  const library = await Library.find({}, 'section abbreviation desc -_id');
+  return res.json(library).status(200);
+});
+
+//get floor(spaces) of library with the given abbreviation
+router.get('/:abbreviation/:floor', async (req, res) => {
+  const queryResult = await Library.find({abbreviation: req.params.abbreviation}, 'file -_id');
+  //TODO: fix json format, change return response
+  //let floor = queryResult[0].file.prostori.filter(prostor => (prostor.nadstropje == req.params.floor));
+
+  return res.json(queryResult[0].file);
+});
+
+//adds new space to file of existing library
+router.post('/:abbreviation', async (req, res) => {
+  const library = await Library.find({abbreviation: req.params.abbreviation}, 'file -_id');
+  let file = library[0].file;
+
+  const newSpace = req.body.space;
+
+  /* TODO: fix json format
+  let prostori = file.prostori;
+  prostori.push(newSpace);
+
+  let result = await Library.updateOne({abbreviation: req.params.abbreviation}, {
+    "file": prostori
+  });
+
+  return res.json(result);*/
+});
+
 export default router;
