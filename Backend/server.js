3e87845cfbc30a1e8cd9f6a1eee63088d5fa6799
Matevz <semprimoznik.matevz@gmail.com>
2022-05-29 10:18:21 +0200

    remove default endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -5,18 +5,12 @@ import librariesRoute from './routes/libraries.js';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || null;
-
 
 const app = express();
 
 app.use(cors());
 
 app.use('/libraries', librariesRoute);
-
-app.get('/', (req, res) => {
-  return res.json('test' + MONGO_URI)
-})
 
 app.listen(PORT, () => {
   createConnection();
