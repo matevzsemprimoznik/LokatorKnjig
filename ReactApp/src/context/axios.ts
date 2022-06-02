7406df48252542c9863664a8a1934286d93c0a3e
Matevz <semprimoznik.matevz@gmail.com>
2022-06-02 20:54:50 +0200

    fix connection from frontend to backend

--- ReactApp/src/context/axios.ts
+++ ReactApp/src/context/axios.ts
@@ -1,8 +1,9 @@
 import Axios from 'axios';
 
 console.log(process.env.REACT_APP_BACKEND_URI)
+const URI = window._env_.REACT_APP_BACKEND_URI != null ? window._env_.REACT_APP_BACKEND_URI : process.env.REACT_APP_BACKEND_URI
 export const libraryApi = Axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URI,
+    baseURL: URI,
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
--- ReactApp/src/types.d.ts
+++ ReactApp/src/types.d.ts
@@ -1,1 +1,9 @@
 declare module "*.glb";
+
+export {};
+
+declare global {
+    interface Window {
+        _env_: any; // 👈️ turn off type checking
+    }
+}
