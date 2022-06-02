8df09e625520fda9708e92b934ec1a132d124c76
Matevz <semprimoznik.matevz@gmail.com>
2022-06-02 21:04:49 +0200

    fix connection from frontend to backend

--- ReactApp/src/context/axios.ts
+++ ReactApp/src/context/axios.ts
@@ -1,9 +1,8 @@
 import Axios from 'axios';
 
 console.log(process.env.REACT_APP_BACKEND_URI)
-const URI = window._env_.REACT_APP_BACKEND_URI != null ? window._env_.REACT_APP_BACKEND_URI : process.env.REACT_APP_BACKEND_URI
 export const libraryApi = Axios.create({
-    baseURL: URI,
+    baseURL: process.env.REACT_APP_BACKEND_URI,
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
