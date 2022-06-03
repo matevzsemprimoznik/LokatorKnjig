cd00d5974af29e0921aaeb52f55d81af13b31431
Matevz <semprimoznik.matevz@gmail.com>
2022-06-03 10:41:30 +0200

    remove testing console log

--- ReactApp/src/context/axios.ts
+++ ReactApp/src/context/axios.ts
@@ -1,6 +1,5 @@
 import Axios from 'axios';
 
-console.log(process.env.REACT_APP_BACKEND_URI)
 export const libraryApi = Axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URI,
     timeout: 30000,
