c3ee06e9e992317ab5c369664e52ed17c9c50030
Matevz <semprimoznik.matevz@gmail.com>
2022-06-08 18:04:25 +0200

    add limit to json payload

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import { createConnection } from './connection.js';
+import {createConnection} from './connection.js';
 import librariesRoute from './routes/libraries.js';
 import udkRoute from './routes/librariesUDK.js';
 import editorRoute from './routes/editor.js';
 import 'dotenv/config';
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
 
 app.use('/libraries', librariesRoute);
 app.use('/udk', udkRoute);
 app.use('/editor', editorRoute);
 
 app.listen(PORT, () => {
-  createConnection();
+    createConnection();
 });
