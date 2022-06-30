cdfc07d2e91f48cad846792405d9719b70791efc
Matevz <semprimoznik.matevz@gmail.com>
2022-06-30 22:27:49 +0200

    refactor code to use refs instead of states for instanced mesh in Model3D.tsx

--- ReactApp/src/3DComponents/Model3D.tsx
+++ ReactApp/src/3DComponents/Model3D.tsx
@@ -1,4 +1,4 @@
-import React, {FC, memo, useEffect, useRef, useState} from 'react';
+import React, {FC, useEffect, useMemo, useRef} from 'react';
 import bookshelfGLB from '../assets/bookshelf.glb';
 import closeBookshelfGLB from '../assets/closeBookshelf.glb';
 import selectedBookshelfGLB from '../assets/selectedBookshelf.glb';
@@ -8,11 +8,7 @@ import {Bookshelf, Position, Room} from '../models/library';
 import Entrances from "./Entrances";
 import {useGLTF} from "@react-three/drei";
 
-import {
-    BufferGeometry,
-    InstancedMesh,
-    Matrix4,
-} from "three";
+import {InstancedMesh, Matrix4,} from "three";
 import {generateMatrixForSignGeometry, size, textGeometry, textMaterial} from "../utils/textBuilder";
 
 interface BookshelvesProps {
@@ -23,117 +19,155 @@ interface BookshelvesProps {
 
 
 const Model3D: FC<BookshelvesProps> = ({selectedUDK, roomData, moveCameraToDoubleClickedPoint}) => {
+    const bookshelfMeshRef = useRef<InstancedMesh | null>(null)
+    const textMeshRef = useRef<InstancedMesh | null>(null)
+    const closeBookshelfMeshRef = useRef<InstancedMesh | null>(null)
+    const selectedBookshelfMeshRef = useRef<InstancedMesh | null>(null)
+    const time = useRef(0)
     const {nodes: bookshelfNodes, materials: bookshelfMaterials}: any = useGLTF(bookshelfGLB);
-    const {nodes: selectedBookshelfNodes, materials: selectedBookshelfMaterials}: any = useGLTF(selectedBookshelfGLB);
-    const {nodes: closeBookshelfNodes, materials: closeBookshelfMaterials}: any = useGLTF(closeBookshelfGLB);
+    const {materials: selectedBookshelfMaterials}: any = useGLTF(selectedBookshelfGLB);
+    const {materials: closeBookshelfMaterials}: any = useGLTF(closeBookshelfGLB);
 
-    const getSelectedUDKPositions = () => {
-        const bookshelves = roomData.bookshelves.filter((bookshelf: Bookshelf, index: number) =>
-            bookshelf.udks.some((udk: any) => udk.toString() === selectedUDK)
+    const getBookshelvesWithSelectedUdk = () => {
+        return roomData.bookshelves.filter((bookshelf, index) =>
+            bookshelf.udks.some((udk: string) => udk.toString() === selectedUDK)
         );
-        const positions: any = [];
-        bookshelves.forEach((bookshelf: Bookshelf, index: number) => {
-            if (
-                index === 0 ||
-                (index !== 0 &&
-                    (bookshelf.position.x !== bookshelves[index - 1].position.x ||
-                        bookshelf.position.z !== bookshelves[index - 1].position.z))
-            )
-                positions.push({x: bookshelf.position.x, z: bookshelf.position.z});
-        });
-        return positions;
-    };
-    const getNewPositionAccordingToRotation = (position: Position, rotationAngle: number) => {
-        rotationAngle = (rotationAngle / 180) * Math.PI;
-        const newPosition = {
-            x: position.x * Math.cos(rotationAngle) + position.z * Math.sin(rotationAngle),
-            y: position.y,
-            z: -position.x * Math.sin(rotationAngle) + position.z * Math.cos(rotationAngle),
-        };
-        return newPosition;
-    };
-
-    const recalculateEntrancePosition = (position: Position, ground: Array<Position>) => {
-        const difference = {
-            x: ground.map(pos => position.x - pos.x),
-            z: ground.map(pos => position.z - pos.z)
-        }
-        console.log(difference)
-        return position
+    }
+    const getBookshelvesCloseToBookshelfWithSelectedUdk = (bookshelvesWithSelectedUdk: Bookshelf[]) => {
+        return roomData.bookshelves.filter((bookshelf, index) => {
+            if (bookshelf.udks.some((udk: string) => udk.toString() === selectedUDK))
+                return false
+            return bookshelvesWithSelectedUdk.some(bookshelfWithSelectedUdk => bookshelfWithSelectedUdk.position.x === bookshelf.position.x && bookshelfWithSelectedUdk.position.z === bookshelf.position.z)
+        })
+    }
+    const getNormalBookshelves = (bookshelvesToRemove: Bookshelf[]) => {
+        return roomData.bookshelves.filter((bookshelf, index) =>
+            !bookshelvesToRemove.some(bookshelfToRemove => bookshelfToRemove.position.x === bookshelf.position.x && bookshelfToRemove.position.y === bookshelf.position.y && bookshelfToRemove.position.z === bookshelf.position.z)
+        )
     }
 
-    const selectedUDKPositions = getSelectedUDKPositions();
+    const getBookshelfMatrix = (bookshelf: Bookshelf) => {
+        const position = {
+            x: (bookshelf.position.x + roomData.center.x) / 20,
+            y: bookshelf.position.y,
+            z: (bookshelf.position.z + roomData.center.z) / 20,
+        }
+        const bookshelfTranslationMatrix = new Matrix4().makeTranslation(position.x, position.y, position.z)
+        const bookshelfRotationMatrix = new Matrix4().makeRotationY(bookshelf.rotation / 180 * Math.PI)
+        return new Matrix4().multiplyMatrices(bookshelfTranslationMatrix, bookshelfRotationMatrix)
 
-    const [bookshelfMesh, setBookshelfMesh] = useState<InstancedMesh>()
-    const [textMesh, setTextMesh] = useState<InstancedMesh>()
+    }
+    const renderBookshelvesWithSelectedUdk = () => {
+        const bookshelvesWithSelectedUdk = getBookshelvesWithSelectedUdk()
+        if (selectedBookshelfMeshRef.current) {
+            bookshelvesWithSelectedUdk.map((bookshelf, index) => {
+                const bookshelfMatrix = getBookshelfMatrix(bookshelf)
+                if (bookshelfMatrix && selectedBookshelfMeshRef.current)
+                    selectedBookshelfMeshRef.current.setMatrixAt(index, bookshelfMatrix)
+            })
+            selectedBookshelfMeshRef.current.instanceMatrix.needsUpdate = true
+
+        }
+    }
+    const renderBookshelvesCloseToBookshelfWithSelectedUdk = () => {
+        const bookshelvesCloseToBookshelfWithSelectedUdk = getBookshelvesCloseToBookshelfWithSelectedUdk(getBookshelvesWithSelectedUdk())
+        if (closeBookshelfMeshRef.current) {
+            bookshelvesCloseToBookshelfWithSelectedUdk.map((bookshelf, index) => {
+                const bookshelfMatrix = getBookshelfMatrix(bookshelf)
+                if (bookshelfMatrix && closeBookshelfMeshRef.current)
+                    closeBookshelfMeshRef.current.setMatrixAt(index, bookshelfMatrix)
+            })
+            closeBookshelfMeshRef.current.instanceMatrix.needsUpdate = true
+
+        }
+    }
+    const renderNormalBookshelves = () => {
+        if (bookshelfMeshRef.current) {
+            normalBookshelves.map((bookshelf, index) => {
+                const bookshelfMatrix = getBookshelfMatrix(bookshelf)
+                if (bookshelfMatrix && bookshelfMeshRef.current)
+                    bookshelfMeshRef.current.setMatrixAt(index, bookshelfMatrix)
+            })
+            bookshelfMeshRef.current.instanceMatrix.needsUpdate = true
+
+        }
+    }
+
+    const renderUdks = () => {
+        if (textMeshRef.current) {
+            let textMeshRefPointer = 0
+            roomData.bookshelves.map((bookshelf, index) => {
+                const position = {
+                    x: (bookshelf.position.x + roomData.center.x) / 20,
+                    y: bookshelf.position.y,
+                    z: (bookshelf.position.z + roomData.center.z) / 20,
+                }
+                const translationMatrix = new Matrix4().makeTranslation(position.x, position.y, position.z)
+                if (bookshelf.udks && bookshelf.udks.length !== 0) {
+                    const splittedText = bookshelf.udks[0].toString().split('')
+                    let offset = -(splittedText.length / 2 * (size.height * 1.4))
+
+                    splittedText.map((sign, index2) => {
+                        const offsetMatrix = new Matrix4().makeTranslation(offset, 0, 0)
+
+                        const matrix = generateMatrixForSignGeometry(sign)
+
+                        matrix.map(textPieceMatrix => {
+                            const multipliedOffsetAndTextPieceMatrix = offsetMatrix.clone().multiply(textPieceMatrix)
+                            const rotatedMatrix = new Matrix4().makeRotationY(bookshelf.rotation / 180 * Math.PI).multiply(multipliedOffsetAndTextPieceMatrix)
+                            const finalMatrix = translationMatrix.clone().multiply(rotatedMatrix)
+                            if (textMeshRef.current)
+                                textMeshRef.current.setMatrixAt(textMeshRefPointer, finalMatrix)
+                            textMeshRefPointer++
+
+                        })
+                        offset += size.height * 1.4
+                    })
+
+                }
+            })
+            textMeshRef.current.instanceMatrix.needsUpdate = true
+
+        }
+    }
+
+    const getNumberOfTextPieces = () => {
+        return roomData.bookshelves.reduce((previousValue, bookshelf) => {
+            let numberOfPiecesForOneUdk = 0
+            if (bookshelf.udks && bookshelf.udks.length !== 0) {
+                const splittedText = bookshelf.udks[0].toString().split('')
+                numberOfPiecesForOneUdk = splittedText.length * 7
+            }
+            return previousValue + numberOfPiecesForOneUdk
+        }, 0)
+    }
+
+    const bookshelvesWithSelectedUdk = useMemo(getBookshelvesWithSelectedUdk, []);
+    const bookshelvesCloseToBookshelvesWithSelectedUdk = useMemo(() => getBookshelvesCloseToBookshelfWithSelectedUdk(bookshelvesWithSelectedUdk), []);
+    const normalBookshelves = useMemo(() => getNormalBookshelves([...bookshelvesWithSelectedUdk, ...bookshelvesCloseToBookshelvesWithSelectedUdk]), [])
+    const numberOfTextPieces = useMemo(getNumberOfTextPieces, [])
 
     useEffect(() => {
-        const t0 = performance.now()
-
-        let count = 0
-        roomData.bookshelves.map((bookshelf, index) => {
-            if (bookshelf.udks && bookshelf.udks.length !== 0) {
-                const splittedText = bookshelf.udks[0].toString().split('')
-                splittedText.map((sign, index2) => count += 7)
-            }
-        })
-        const bookshelfInstanceMesh = new InstancedMesh(bookshelfNodes.Cube.geometry, bookshelfMaterials['CubeMaterial'], roomData.bookshelves.length)
-        const textInstanceMesh = new InstancedMesh(textGeometry, textMaterial, count)
-
-
-        let matrixPosition = 0
-        roomData.bookshelves.map((bookshelf, index) => {
-            const position = {
-                x: (bookshelf.position.x + roomData.center.x) / 20,
-                y: bookshelf.position.y,
-                z: (bookshelf.position.z + roomData.center.z) / 20,
-            }
-            const bookshelfTranslationMatrix = new Matrix4().makeTranslation(position.x, position.y, position.z)
-            const bookshelfRotationMatrix = new Matrix4().makeRotationY(bookshelf.rotation / 180 * Math.PI)
-            const bookshelfMatrix = new Matrix4().multiplyMatrices(bookshelfTranslationMatrix, bookshelfRotationMatrix)
-
-            bookshelfInstanceMesh.setMatrixAt(index, bookshelfMatrix)
-
-            const translationMatrix = new Matrix4().makeTranslation(position.x, position.y, position.z)
-
-            if (bookshelf.udks && bookshelf.udks.length !== 0) {
-                const splittedText = bookshelf.udks[0].toString().split('')
-                let offset = -(splittedText.length / 2 * (size.height * 1.4))
-
-                splittedText.map((sign, index2) => {
-                    const offsetMatrix = new Matrix4().makeTranslation(offset, 0, 0)
-
-                    const matrix = generateMatrixForSignGeometry(sign)
-
-                    matrix.map((textPieceMatrix: Matrix4, index3: number) => {
-                        const multipliedOffsetAndTextPieceMatrix = offsetMatrix.clone().multiply(textPieceMatrix)
-                        const rotatedMatrix = new Matrix4().makeRotationY(bookshelf.rotation / 180 * Math.PI).multiply(multipliedOffsetAndTextPieceMatrix)
-                        const finalMatrix = translationMatrix.clone().multiply(rotatedMatrix)
-                        textInstanceMesh.setMatrixAt(matrixPosition, finalMatrix)
-                        matrixPosition++
-
-                    })
-                    offset += size.height * 1.4
-                })
-
-            }
-        })
-
-        setBookshelfMesh(bookshelfInstanceMesh)
-        setTextMesh(textInstanceMesh)
+        renderNormalBookshelves()
+        renderBookshelvesWithSelectedUdk()
+        renderBookshelvesCloseToBookshelfWithSelectedUdk()
+        renderUdks()
     }, [roomData])
 
 
     return (
         <>
-            {/* @ts-ignore*/}
-            {bookshelfMesh && <instancedMesh {...bookshelfMesh} />}
-            {/* @ts-ignore*/}
-            {textMesh && <instancedMesh {...textMesh} />}
+            <instancedMesh ref={bookshelfMeshRef}
+                           args={[bookshelfNodes.Cube.geometry, bookshelfMaterials['CubeMaterial'], normalBookshelves.length]}/>
 
+            <instancedMesh ref={textMeshRef} args={[textGeometry, textMaterial, numberOfTextPieces]}/>
+            <instancedMesh ref={closeBookshelfMeshRef}
+                           args={[bookshelfNodes.Cube.geometry, closeBookshelfMaterials['CubeMaterial'], bookshelvesCloseToBookshelvesWithSelectedUdk.length]}/>
+            <instancedMesh ref={selectedBookshelfMeshRef}
+                           args={[bookshelfNodes.Cube.geometry, selectedBookshelfMaterials['CubeMaterial'], bookshelvesWithSelectedUdk.length]}/>
             <Entrances
                 entrances={roomData.entrances}
-                position={{x: roomData.center.x / 20, y: roomData.center.y / 20, z: roomData.center.z / 20,}}
+                position={{x: roomData.center.x / 20, y: roomData.center.y / 20, z: roomData.center.z / 20}}
             />
 
 
